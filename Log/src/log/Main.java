8a690213ef5d7e4b2b08a95ffa0e42bb8e6f31bc
Saleh Itayem <salehktaweel@gmail.com>
2024-07-21 22:42:30 +0300

    Add files via upload

---
+++ Log/src/log/Main.java
@@ -0,0 +1,13 @@
+package log;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Max l = Max.getInstance();
+		
+		System.out.println(l.getMax(50, 70));
+		
+	}
+
+}
---
+++ Log/src/log/Max.java
@@ -0,0 +1,35 @@
+package log;
+
+public class Max {
+	// single instance 
+	private static Max instance;
+	
+	// private constructor to prevent creating another instance 
+	private Max() {};
+		
+	// get math method to find the greater integer :D
+	public int getMax(int x, int y) {
+		if (x > y) {
+			return x;
+		}
+		return y;
+	}
+		
+	// public method to give access to the instance :X
+	public static Max getInstance() {
+		
+		if (instance == null) {
+			synchronized (Max.class ) {
+				if (instance == null) {
+					instance = new Max();
+				}
+			}
+		}
+		return instance;
+	}
+	
+	// method to log MSGS
+	public void log(String part) {
+		System.out.println(part);
+	}
+}
